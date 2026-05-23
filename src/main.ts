c5bbe2f759741c5fe20c9b65593e54a446790567
Santiago107R <santiagorobles107k@gmail.com>
2026-05-23 17:45:50 -0300

    docs fixed swagger

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/docs', app, documentFactory);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
